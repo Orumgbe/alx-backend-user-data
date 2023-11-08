1be63d2f2c84806b7beee948075d896780ca12be
Orumgbe <orumgbeugbekile@gmail.com>
2023-11-08 05:13:57 -0800

    forbidden response added

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,6 +14,18 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+@app.errorhandler(401)
+def not_authorized(error) -> str:
+    """Unauthorized error handler"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def not_allowed(error) -> str:
+    """Forbidden error handler"""
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -21,13 +33,7 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
-def not_authorized(error) -> str:
-    """Unauthorized error handler"""
-    return jsonify({"error": "Unauthorized"}), 401
-
-
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(debug=True, host=host, port=port)
