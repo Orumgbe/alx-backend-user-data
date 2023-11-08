cb5fe6c6fb1aa20146c42879cb9c5509506fdb73
Orumgbe <orumgbeugbekile@gmail.com>
2023-11-08 05:01:58 -0800

    correcting route in app_views

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,7 +22,7 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
+def not_authorized(error) -> str:
     """Unauthorized error handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
