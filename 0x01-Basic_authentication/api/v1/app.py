6af572f9b06ec7dd0e8c1a0b21cf8314a1c895d5
Orumgbe <orumgbeugbekile@gmail.com>
2024-04-23 06:12:07 -0700

    adding choice between auth and basic_auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,7 +3,6 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.auth.auth import Auth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -13,7 +12,16 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = Auth()
+auth = None
+AUTH_TYPE = getenv("AUTH_TYPE")
+
+
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+if AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
