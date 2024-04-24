d25a025fff05506922e3431681a67eb1688a00bd
Orumgbe <orumgbeugbekile@gmail.com>
2024-04-24 03:45:56 -0700

    extract users username and password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,15 @@ class BasicAuth(Auth):
         except BaseException:
             return None
         return auth_base64_dec.decode('utf-8')
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """Return user email and password from the Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if decoded_base64_authorization_header.find(':') == -1:
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(":"))        
